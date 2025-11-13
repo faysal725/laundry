fa2862899d94e06749a0c45f81e6cb1772c12510
fahmid munshi <fahmidmunshi2004@gmail.com>
2025-11-13 16:26:06 +0600

    service-done

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,6 +1,5 @@
-function toggleSidebar() {
-      const sidebar = document.getElementById('sidebar');
-      const mainContent = document.getElementById('main-content');
-      sidebar.classList.toggle('open');
-      mainContent.classList.toggle('ml-64');
-    }
+document.querySelectorAll(".select-dropdown li").forEach(li => {
+  li.onclick = () =>
+    document.querySelector(".select-dropdown > b").textContent =
+    li.querySelector("b").textContent;
+});
